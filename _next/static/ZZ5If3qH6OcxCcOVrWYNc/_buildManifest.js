cc8a02a283a18709733d86eaa54fa397b074dc19
juliaselk <102349660+juliaselk@users.noreply.github.com>
2022-12-05 02:14:10 +0000

    Deploying to public from @ juliaselk/juliaselk@f7cadd1352684bc72b6a2b6c3eccee2fb139b7f6 🚀

---
+++ _next/static/ZZ5If3qH6OcxCcOVrWYNc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4979603ec75d7649.css","static/chunks/pages/index-fc03b8727c0cef73.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
